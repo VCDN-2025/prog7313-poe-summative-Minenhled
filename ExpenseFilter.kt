8f7e322ce558d236a384dbfc7fface7245a28bcf
Minenhled <104920287+Minenhled@users.noreply.github.com>
2025-06-09 13:30:35 +0200

    Update ExpenseFilter.kt

--- ExpenseFilter.kt
+++ ExpenseFilter.kt
@@ -66,4 +66,21 @@ object ExpenseFilterUtils {
             endDate = DateUtils.getEndOfMonth(today)
         )
     }
+
+
+    //Google. (n.d.). Android developers. https://developer.android.com
+//
+//Google. (n.d.). Android Studio user guide. https://developer.android.com/studio/intro
+//
+//Google. (n.d.). Android Codelabs. https://developer.android.com/codelabs
+//
+//Google. (n.d.). Android API reference. https://developer.android.com/reference
+//
+//Stack Overflow. (n.d.). Android tag – Stack Overflow. https://stackoverflow.com/questions/tagged/android
+//
+//Google. (n.d.). Android · GitHub. https://github.com/android
+//
+//JetBrains. (n.d.). Kotlin Playground. https://play.kotlinlang.org
+//
+//JetBrains. (n.d.). JetBrains Academy. https://www.jetbrains.com/academy/
 }
