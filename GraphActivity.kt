c93bf7299342e720ea03d3de24db3b139567e7ee
Minenhled <104920287+Minenhled@users.noreply.github.com>
2025-06-09 13:17:45 +0200

    Update GraphActivity.kt

--- GraphActivity.kt
+++ GraphActivity.kt
@@ -24,3 +24,36 @@ class GraphActivity : AppCompatActivity() {
         return true
     }
 }
+//🔗 Android Developer Portal  
+//   https://developer.android.com  
+//   (Official documentation, API guides, and Jetpack libraries)
+//
+//🔗 Android Studio User Guide  
+//   https://developer.android.com/studio/intro  
+//   (Installation, setup, Gradle, and emulator instructions)
+//
+//🔗 Android Codelabs  
+//   https://developer.android.com/codelabs  
+//   (Hands-on tutorials for Room, ViewModel, Compose, etc.)
+//
+//🔗 Android API Reference  
+//   https://developer.android.com/reference  
+//   (Full SDK API list for Java & Kotlin)
+//
+//🔗 Stack Overflow - Android  
+//   https://stackoverflow.com/questions/tagged/android  
+//   (Community Q&A, error troubleshooting)
+//
+//🔗 Android GitHub Samples  
+//   https://github.com/android  
+//   (Official sample projects for Android features)
+//
+//🔗 Kotlin Playground  
+//   https://play.kotlinlang.org  
+//   (Practice Kotlin syntax online)
+//
+//🔗 JetBrains Academy  
+//   https://www.jetbrains.com/academy/  
+//   (Courses for Kotlin, Java, Android development)
+
+
